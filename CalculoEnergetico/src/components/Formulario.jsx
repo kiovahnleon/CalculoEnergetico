f3210453594cddeb8f1136e04d0825fe127fc7e6
kiovahnleon <al20760618@ite.edu.mx>
2022-05-25 22:27:19 -0700

    for -> htmlfor

--- CalculoEnergetico/src/components/Formulario.jsx
+++ CalculoEnergetico/src/components/Formulario.jsx
@@ -66,7 +66,7 @@ const Formulario = (setPersona) => {
                 <div>
                     <label htmlFor="genero">Genero</label><br />
                     <input type="radio" id="masculino" value={sexo} onChange={(e) => setSexo(e.target.value)} />
-                    <label for="html"> Masculino</label>
+                    <label htmlfor="sexo"> Masculino</label>
                     <input type="radio" id="femenino" value={sexo} onChange={(e) => setSexo(e.target.value)} />
                     <label for="css"> Femenino</label><br />
 
